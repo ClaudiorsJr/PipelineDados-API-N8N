fb3674d4f2815cf8f5d0f75fb1706441c1645f51
ClaudiorsJr <claudiorsjunior@outlook.com>
2025-08-18 19:47:09 -0300

    Commodities Added

--- GetCommodities.py
+++ GetCommodities.py
@@ -1,7 +1,26 @@
 import yfinance as yf
+from datetime import datetime
 
-ticker = yf.Ticker("GC=F")
 
-df = ticker.history(period="1d", interval="1m")
 
-print(df)
+def get_commodities():
+
+    '''Obter as cotações de commodities'''
+    try:
+        # Ouro
+        df_commodities = yf.Ticker("GC=F").history(period="1d", interval="1m")[['Close']].tail(1)
+        df_commodities = df_commodities.rename(columns = {'Close': 'preco'})
+        df_commodities['ativo'] = 'GC=F'
+        df_commodities['moeda'] = 'USD'
+        df_commodities['data_coleta'] = datetime.now()
+        df_commodities = df_commodities[['ativo', 'preco', 'moeda', 'data_coleta']]
+
+        return df_commodities
+    
+    except Exception as e:
+        print(f"Erro ao obter as commodities: {e}")
+        return None
+
+commodities_data = get_commodities()
+
+print(commodities_data)
